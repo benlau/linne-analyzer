0a5ee2ed1888fdb7f5e305e78adb067fe4e54f64
Ben Lau <xbenlau@gmail.com>
2013-03-16 14:59:19 +0800

    zhuyin/phonetic.py - Mandarin don't have pronounce with only consonant.

--- src/linne/analyzer/zhuyin/phonetic.py
+++ src/linne/analyzer/zhuyin/phonetic.py
@@ -9,15 +9,24 @@ class Phonetic:
    
     def __init__(self,symbol):
         """
-        @type phonetics: list
-        @param phonetics: list of zhu yin symbol 
+        @type symbol: string
+        @param symbol: The phonetic of a Chinese character formed by zhu yin symbol 
         """
-        self.consonant = symbol[0]
-        self.vowel = symbol[1:len(symbol)]
+        if len(symbol) == 1:
+            self.consonant = u''
+            self.vowel = symbol[0] # Mandarin don't have pronounce with only consonant
+        else:
+            self.consonant = symbol[0]
+            self.vowel = symbol[1:len(symbol)]
+
         self.points = []
         
     def __unicode__(self):
-        return self.consonant + u''.join(self.vowel)
+        ret = u''
+        if len(self.consonant) > 0:
+            ret = ret + self.consonant
+        ret = ret + u''.join(self.vowel)
+        return ret
         
     def __str__(self):
         return unicode(self).encode("utf-8")
@@ -27,7 +36,9 @@ class Phonetic:
         
         @rtype: list
         """
-        ret = [self.consonant]
+        ret = []
+        if len(self.consonant) > 0:
+            ret.append(self.consonant)
         if len(self.vowel) > 0:
             ret.append(self.vowel[0])
         return ret
@@ -35,8 +46,8 @@ class Phonetic:
     def toLabel(self):
         """Convert to Audacity label format"""
         ret = []
-        if len(self.vowel) == 0:
-            ret.append([self.points[0],self.points[1],self.consonant] )
+        if len(self.consonant) == 0:
+            ret.append([self.points[0],self.points[1],self.vowel] )
         else:
             ret.append([self.points[0],self.points[1],"[" + self.consonant + "]"] )
             ret.append([self.points[1],self.points[2],"[" + u''.join(self.vowel)+ "]"] )
